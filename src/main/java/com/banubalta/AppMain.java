8c85be482ac2214c0f057f6980ca98a1c2118e1a
Banu balta <nbanu.balta@gmail.com>
2024-01-03 20:54:58 +0300

    Initial commit

---
+++ src/main/java/com/banubalta/AppMain.java
@@ -0,0 +1,24 @@
+package com.banubalta;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+   // verilen bir metni tersine cevirip
+   // bize donduren bir method olusturun
+   // "Java ile kod yaz" cumlesini tersten yazdirin
+        String metin = "Java çok güzeldir ama ben daha güzelim";
+
+        String tersMetin ="";
+
+        for (int i = metin.length()-1; i >=0 ; i--) {
+            tersMetin += metin.charAt(i);
+        }
+
+        System.out.println(tersMetin);
+
+    }}
+
+
+
+
+
---
+++ src/main/java/com/banubalta/Main.java
@@ -0,0 +1,20 @@
+package com.banubalta;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Soru 1- Verilen bir int array’in tum elemanlarini 2 artirin
+
+        int[] arr = {5,1,7,9,2};
+        for (int i = 0; i < arr.length ; i++) {
+
+            arr[i] =arr[i]+2;
+
+
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }}
